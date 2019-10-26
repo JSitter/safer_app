ddc2497e6dca846f6e3d58a7839a6cbaacb2e9f2
Justin Sitter <justin@jaytria.com>
2019-10-26 15:26:32 -0700

    update App and styles

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import './styles.css';
+import Login from './Login.js';
+import Splash from './Splash.js';
+import Signup from './Signup';
+import Footer from './Footer.js';
+import Dashboard from './Dashboard.js';
+import Map from './Map.js';
+require('dotenv').config()
 
+function checkLogin(user, pass, setLogin){
+  setLogin("authenticated");
+}
 
 function App() {
+
+  const [login, setLogin] = useState('guest');
+  const [showLogin, updateLogin] = useState('hidden');
+
   return (
     <div className="App">
-      <header className="bg-header parallax header-bg">
-        <section ><a href="#" className="header-logo logo-color">Safer App</a></section>
+      <header className="parallax header-bg" >
+        <a href="#" className="header-logo logo-color logo-section">Safer App</a>
       </header>
       <nav className="navbar sticky nav-color">
 
         <ul className="nav-title">
-          <li><a href="http://Some Site.com"><h1>Safer App</h1></a></li>
+          <li><a href="http://Some Site.com"><h1 className="nav-site-link">Safer App</h1></a></li>
         </ul>
+        
         <section className="nav-section" >
           <ul>
             <li>
@@ -26,67 +41,16 @@ function App() {
               <a href="https://jaytria.com">About Us</a>
             </li>
           </ul>
-        </section>
-
-        </nav>
-        <section class="hero-block">
-  <h2>Be Safer with Safer App</h2>
-  <p>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-  </p>
-</section>
-<footer>
-    <section class="col">
-      <span class="footer-title">
-        <a href="http://google.com"><h2>Title One</h2></a>
-      </span>
-      <ul>
-        <li>
-        <a href="http://google.com">Title One</a>
-        </li>
-        <li>
-            <a href="http://google.com">Link Two</a>
-        </li>      
-        <li>
-            <a href="http://google.com">Link Three</a>
-        </li>
-      </ul>
-    </section>
-
-    <section class="col">
-        <span class="footer-title">
-          <a href="http://jkjoogle.com"><h2>Title One</h2></a>
-        </span>
-        <ul>
-          <li>
-            <a href="http://xazabbad.com">Link One</a>
-          </li>
-          <li>
-              <a href="http://xyzabc.com">Link Two</a>
-          </li>      
-          <li>
-              <a href="http://google.com">Link Three</a>
-          </li>
-        </ul>
-      </section>
-
-      <section class="col">
-          <span class="footer-title">
-            <h2>Title One</h2>
-          </span>
-          <ul>
-            <li>
-              <a href="http://google.com">Link One</a>
-            </li>
-            <li>
-                <a href="http://google.com">Link Two</a>
-            </li>      
-            <li>
-                <a href="http://google.com">Link Three</a>
-            </li>
+          <ul className="nav-right">
+            <li><a href="#" onClick={()=>{updateLogin('login')}}>Login</a></li>
           </ul>
+          
         </section>
-  </footer>
+        </nav>
+    {login === 'guest'? <Splash/> : <Dashboard/>}
+    {showLogin === "login"? <Login visibility={showLogin} setVis={updateLogin} checkLogin={checkLogin}/>: showLogin === 'signup'? <Signup visibility={showLogin} setVis={updateLogin} setLogin={setLogin}/>: ""}
+      <Map/>
+      <Footer/>
     </div>
   );
 }
